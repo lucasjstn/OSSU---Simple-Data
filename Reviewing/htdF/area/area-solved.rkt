6b105b5bed0822563eafea19c53a308b01b8206f
lucasjstn <lucas7justino@gmail.com>
2022-11-03 06:16:17 -0300

    reviewing area functions for htdf

---
+++ Reviewing/htdF/area/area-solved.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname area-solved) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Number -> Number
+;; given number produce a squared number
+
+(check-expect (area 3) 9)
+(check-expect (area 2.1) (* 2.1 2.1))
+
+;(define (area s) 0) ;stub
+
+(define (area s)
+  (* s s))
